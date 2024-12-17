89d464f6610d54289a8507afcc083ba408190fe6
Izabella Balogh <n01716105@humber.ca>
2024-12-17 09:36:14 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+const elem = document.createElement("input");
+elem.type = "file";
+elem.addEventListener("cancel", () => {
+  console.log("Cancelled.");
+});
+elem.addEventListener("change", () => {
+  if (elem.files.length == 1) {
+    console.log("File selected: ", elem.files[0]);
+  }
+});
+elem.click();
+function formSubmit() {
+    document.forms["myForm"].submit();
+}
